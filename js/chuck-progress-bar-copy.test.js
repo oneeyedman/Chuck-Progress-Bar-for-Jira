f9daface82fb72f4dc569d151edb7c6413491517
Carlos Mañas <carlos@spacenomads.com>
2021-03-13 16:02:43 +0100

    Refactor progress bar code

--- js/chuck-progress-bar-copy.test.js
+++ js/chuck-progress-bar-copy.test.js
@@ -1,6 +1,67 @@
 /* eslint-disable no-undef */
-import {camelcase} from './chuck-progress-bar.js';
+import { 
+	camelcase,
+	getColumns,
+	populateKanban,
+	getTotalIssues,
+	getIssueSummary,
+	deleteUserComponent,
+	drawChuckProgressBar } from './chuck-progress-bar.js';
 
+document.body.innerHTML = `
+		<div id="ghx-pool-column">
+    <div class="ghx-column-headers">
+      <div class="ghx-column" data-id="1">
+        <h2 data-tooltip="To Do">To do</h2>
+      </div>
+      <div class="ghx-column" data-id="2">
+        <h2 data-tooltip="In Progress">In Progress</h2>
+      </div>
+      <div class="ghx-column" data-id="3">
+        <h2 data-tooltip="Blocked">Blocked</h2>
+      </div>
+      <div class="ghx-column" data-id="4">
+        <h2 data-tooltip="MR Pending">MR Pending</h2>
+      </div>
+      <div class="ghx-column" data-id="5">
+        <h2 data-tooltip="Done">Done</h2>
+      </div>
+    </div>
+    <ul class="ghx-columns">
+      <li class="ghx-column" data-column-id="1">
+        <div class="ghx-issue">Issue 1</div>
+        <div class="ghx-issue">Issue 2</div>
+        <div class="ghx-issue">Issue 3</div>
+      </li>
+      <li class="ghx-column" data-column-id="2">
+        <div class="ghx-issue">Issue 1</div>
+
+      </li>
+      <li class="ghx-column" data-column-id="3">
+        <div class="ghx-issue">Issue 1</div>
+        <div class="ghx-issue">Issue 2</div>
+      </li>
+      <li class="ghx-column" data-column-id="4">
+      </li>
+      <li class="ghx-column" data-column-id="5">
+        <div class="ghx-issue">Issue 1</div>
+        <div class="ghx-issue">Issue 2</div>
+        <div class="ghx-issue">Issue 3</div>
+        <div class="ghx-issue">Issue 4</div>
+        <div class="ghx-issue">Issue 5</div>
+        <div class="ghx-issue">Issue 6</div>
+        <div class="ghx-issue">Issue 7</div>
+        <div class="ghx-issue">Issue 8</div>
+        <div class="ghx-issue">Issue 9</div>
+        <div class="ghx-issue">Issue 10</div>
+      </li>
+    </ul>
+  </div>`;
+const poolColumn = document.querySelector('#ghx-pool-column');
+const headerCols = poolColumn.querySelector('.ghx-column-headers');
+const domIssues = poolColumn.querySelectorAll('.ghx-issue').length;
+const domToDoIssues = poolColumn.querySelectorAll('[data-column-id="1"] .ghx-issue').length;
+const domDoneIssues = poolColumn.querySelectorAll('[data-column-id="5"] .ghx-issue').length;
 describe('CamelCase converter', () => {
 	const inputs = [
 		{
@@ -29,5 +90,129 @@ describe('CamelCase converter', () => {
 			expect(camelcase(input.label)).toBe(input.result);
 		});
 	});
+});
 
+
+
+
+
+describe('Kanban object', () => {
+	test('Should return kanban object: { toDo: {  el, dataId } }', () => {
+		expect(getColumns(headerCols)).toEqual(
+			expect.objectContaining({
+				toDo: expect.objectContaining({
+					el: expect.any(Object),
+					dataId: expect.any(String)
+				}),
+				inProgress: expect.objectContaining({
+					el: expect.any(Object),
+					dataId: expect.any(String)
+				}),
+				blocked: expect.objectContaining({
+					el: expect.any(Object),
+					dataId: expect.any(String)
+				}),
+				mrPending: expect.objectContaining({
+					el: expect.any(Object),
+					dataId: expect.any(String)
+				}),
+				done: expect.objectContaining({
+					el: expect.any(Object),
+					dataId: expect.any(String)
+				})
+			})
+		);
+	});
+
+	test('Should populate Kanban object ', () => {
+		const kanban = getColumns(headerCols);
+		populateKanban(kanban);
+		expect(kanban).toEqual(
+			expect.objectContaining({
+				toDo: expect.objectContaining({
+					el: expect.any(Object),
+					dataId: expect.any(String),
+					issues: expect.any(Number),
+				}),
+				inProgress: expect.objectContaining({
+					el: expect.any(Object),
+					dataId: expect.any(String),
+					issues: expect.any(Number),
+				}),
+				blocked: expect.objectContaining({
+					el: expect.any(Object),
+					dataId: expect.any(String),
+					issues: expect.any(Number),
+				}),
+				mrPending: expect.objectContaining({
+					el: expect.any(Object),
+					dataId: expect.any(String),
+					issues: expect.any(Number),
+				}),
+				done: expect.objectContaining({
+					el: expect.any(Object),
+					dataId: expect.any(String),
+					issues: expect.any(Number),
+				})
+			})
+		);
+	});
+});
+
+
+
+
+
+describe('Generate an issue summary', () => {
+	test(`There are 16 unique issues`, () => {
+		const kanban = getColumns(headerCols);
+		populateKanban(kanban);
+		const totalIssues = getTotalIssues(kanban);
+		expect(totalIssues).toBe(domIssues);
+	});
+
+	test(`There are 3 issues out of a total of 16.`, () => {
+		const kanban = getColumns(headerCols);
+		populateKanban(kanban);
+		const summary = getIssueSummary(kanban);
+		expect(summary).toEqual(expect.objectContaining({
+			done: domDoneIssues,
+			pending: domToDoIssues,
+			total: domIssues
+		}));
+	});
+});
+
+
+
+
+describe('Paintint the Chuck Progress bar', () => {
+	test('Delete previous progress bar', () => {
+		poolColumn.insertAdjacentHTML('afterbegin', '<section class="chuck-progress-bar">:)</div>');
+		deleteUserComponent('.chuck-progress-bar');
+
+		const cpb = document.querySelector('.chuck-progress-bar');
+		expect(cpb).toBe(null);
+	});
+
+	test('Successfully paint the progress bar element', () => {
+		const kanban = getColumns(headerCols);
+		populateKanban(kanban);
+		const summary = getIssueSummary(kanban);
+		drawChuckProgressBar(poolColumn, summary, 'rainbow');
+
+		const cpb = document.querySelector('.chuck-progress-bar');
+		expect(cpb).toEqual(expect.any(Object));
+	});
+
+	test('Successfully paint only 1 progress bar element', () => {
+		const kanban = getColumns(headerCols);
+		populateKanban(kanban);
+		const summary = getIssueSummary(kanban);
+		drawChuckProgressBar(poolColumn, summary, 'rainbow');
+
+		const cpb = document.querySelectorAll('.chuck-progress-bar');
+		expect(cpb.length).toBe(1);
+	});
+	
 });
--- js/chuck-progress-bar.js
+++ js/chuck-progress-bar.js
@@ -1,13 +1,3 @@
-'use strict';
-
-const kanban = {
-	toDo: {},
-	inProgress: {},
-	blocked: {},
-	mrPending: {},
-	done: {}
-};
-
 function camelcase(str) {
 	const parts = str.toLowerCase().split(' ');
 	for (let i = 0; i < parts.length; i++) {
@@ -20,42 +10,72 @@ function camelcase(str) {
 	return parts.join('');
 }
 
-function getTitles(list) {
+
+
+
+
+function getColumns(list) {
 	const cols = list.querySelectorAll('.ghx-column');
-	const result = [...cols].map(header => {
-		const element = header.querySelector('h2');
-		const title = header.querySelector('h2').innerText;
-		return {
-			title,
-			element
-		};
+	const result = {};
+	cols.forEach(col => {
+		const element = col.querySelector('h2');
+		const dataId = col.dataset.id;
+		const title = col.querySelector('h2').textContent;
+		result[camelcase(title)] = {el: element, dataId};
 	});
 	return result;
 }
 
-function populateColumnsData(cols, obj) {
-	const colData = cols.querySelectorAll('.ghx-column');
-	colData.forEach((col, index) => {
+
+
+
+function populateKanban(obj) {
+	for (const prop in obj) {
+		const {dataId} = obj[prop];
+		const col = document.querySelector(`[data-column-id="${dataId}"]`);
 		const issues = col.querySelectorAll('.ghx-issue').length;
-		obj[index].issues = issues;
-	});
+		obj[prop].issues = issues;
+	}
+	return obj;
 }
 
-function deleteUserComponent(selector) {
-	const el = document.querySelectorAll(selector);
-	el && el.forEach(el => el.remove());
+
+
+
+function getTotalIssues(obj) {
+	let result = 0;
+	for (const prop in obj) {
+		result += obj[prop].issues;
+	}
+	return result;
 }
 
-function drawProgressBar(data, element, theme = 'default') {
+
+
+
+function getIssueSummary(obj) {
+	const total = getTotalIssues(obj);
+	const {toDo, done} = obj;
+	const pending = toDo.issues;
+	const doneIssues = done.issues;
+	return { pending, done: doneIssues, total};
+}
+
+
+
+
+function drawChuckProgressBar(container, obj, theme='default') {
 	const themes = {
 		'default': 'background-color:#0052cc;',
 		'rainbow': 'background: linear-gradient(to right, red, orange, yellow, green, blue, purple);',
 		'pride': 'background: linear-gradient(to right, red 0%, red 16.66666666%, orange 16.66666666%, orange 33.33333333%, yellow 33.33333333%, yellow 50%, green 50%, green 66.66666666%, blue 66.66666666%, blue 83.33333333%, purple 83.33333333%, purple 100%);'
 	};
-	const bg = themes[theme];
+	const bg = themes[theme] || themes['default'];
+
 	deleteUserComponent('.chuck-progress-bar');
-	const totalIssues = data.reduce((total, col) => total + col.issues, 0);
-	const percentage = data[4].issues * 100 / totalIssues;
+	
+	const {done, total} = obj;
+	const percentage = done * 100 / total;
 	const progressBar = `<section class="chuck-progress-bar" style="background-color:#f5f4f7;border-radius: 4px; padding: 10px 5px;margin-bottom: 7px;">
 		<h1 class="chuck-progress-bar__title" style="color: #5e6c84;font-size:.85714286em;padding:5px 0 15px 5px;line-height: 1.33333333;text-transform: uppercase;font-weight: 400;overflow: hidden;text-overflow: ellipsis;white-space: nowrap;">Chuck Progress bar FTW</h1>
 		<div class="chuck-progress-bar__track" style="background-color:#e5e6e7;border-radius: 2px;overflow: hidden;">
@@ -63,31 +83,47 @@ function drawProgressBar(data, element, theme = 'default') {
 			<div class="chuck-progress-bar__current" style="${bg}border-radius: 2px;box-shadow:0 0 5px rgba(0,0,0,.2);height:30px;width:${percentage}%"></div>
 		</div>
 	</section>`;
-	element.insertAdjacentHTML('afterbegin', progressBar);
-
-
+	container.insertAdjacentHTML('afterbegin', progressBar);
 }
 
-function getTotalIssues() {
+
+
+
+
+function deleteUserComponent(selector) {
+	const el = document.querySelectorAll(selector);
+	el && el.forEach(el => el.remove());
+}
+
+
+
+
+
+function initKanban() {
 	const poolColumn = document.querySelector('#ghx-pool-column');
 	if (poolColumn) {
 		const headerCols = poolColumn.querySelector('.ghx-column-headers');
-		const columns = getTitles(headerCols);
-		const taskColumns = document.querySelector('.ghx-columns');
-		populateColumnsData(taskColumns, columns);
-		const [todo, inProgress, blocked, mrPending, done] = columns;
-		const totalIssues = columns.reduce((total, col) => total + col.issues, 0);
+		const kanban = getColumns(headerCols);
+
+		populateKanban(kanban);
+		const {pending, total} = getIssueSummary(kanban);
 
 		// Write progressbar
-		drawProgressBar(columns, poolColumn, 'rainbow');
+		drawChuckProgressBar(poolColumn, getIssueSummary(kanban), `prides`);
 		// write column totals
-
 		console.group('TASKS');
-		console.log(`${todo.issues}/${totalIssues} \nDONE: ${done.issues}`);
+		console.log(`${pending}/${total} \nDONE: ???`);
 		console.groupEnd();
 	}
 }
 
-getTotalIssues();
+initKanban();
 
-export { camelcase };
+export { 
+	camelcase,
+	getColumns,
+	populateKanban,
+	getTotalIssues,
+	getIssueSummary,
+	deleteUserComponent,
+	drawChuckProgressBar };
